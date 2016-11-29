a5a6a69ee18be1630c3a7fa3bdc97f775bc40d54
Piotr Wittchen <piotr@wittchen.biz.pl>
2016-11-29 21:59:34 +0100

    adding new test: testExtractZipValueFromJson

--- test.sh
+++ test.sh
@@ -6,7 +6,21 @@ TEST_MODE=true
 . ./ydownloader
 echo "RUNNING UNIT TESTS..."
 
-JSON_RESPONSE_STUB='{
+JSON_STUB_RESPONSE_WITH_URL_ENDPOINT='{
+  "results": [
+    {
+      "uri": "https://artifactory-url.com/commerce-suite-sources.zip"
+    },
+    {
+      "uri": "https://artifactory-url.com/commerce-suite.pom"
+    },
+    {
+      "uri": "https://artifactory-url.com/commerce-suite.zip"
+    }
+  ]
+}'
+
+JSON_STUB_RESPONSE_WITH_DETAILS='{
   "repo": "hybris-release",
   "path": "/path/to/commerce-suite.zip",
   "created": "2016-11-28T15:35:36.878+01:00",
@@ -28,9 +42,21 @@ JSON_RESPONSE_STUB='{
   "uri": "https://artifactory-url.com/commerce-suite.zip"
 }'
 
+testExtractZipValueFromJson() {
+  # given
+  RESPONSE_WITH_URL_ENDPOINT=$JSON_STUB_RESPONSE_WITH_URL_ENDPOINT
+  expectedValue='https://artifactory-url.com/commerce-suite.zip'
+
+  # when
+  actualValue=$(echo $RESPONSE_WITH_URL_ENDPOINT | extractValueFromJson zip)
+
+  # then
+  assertEquals $expectedValue $actualValue
+}
+
 testGetDownloadUriFromResponse() {
   # given
-  RESPONSE_URL_WITH_DETAILS=$JSON_RESPONSE_STUB
+  RESPONSE_URL_WITH_DETAILS=$JSON_STUB_RESPONSE_WITH_DETAILS
   expectedValue='https://artifactory-url.com/commerce-suite.zip'
 
   # when
@@ -42,7 +68,7 @@ testGetDownloadUriFromResponse() {
 
 testGetReleaseTimeFromResponse() {
   # given
-  RESPONSE_URL_WITH_DETAILS=$JSON_RESPONSE_STUB
+  RESPONSE_URL_WITH_DETAILS=$JSON_STUB_RESPONSE_WITH_DETAILS
   expectedValue='2016-11-28T15:35:00.685+01:00'
 
   # when
